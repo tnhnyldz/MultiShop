1f52eaed7713eb9bf1c7e15f2a4bf1632f4fea9f
Tnhnyildiz <tunahanyildiz@outlook.it>
2024-12-04 23:57:37 +0300

    order microservices and CQRS pattern

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System.Data;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.Addresshandlers
+{
+	public class UpdateAddressCommandHandler
+	{
+		private readonly IRepository<Address> _repository;
+
+		public UpdateAddressCommandHandler(IRepository<Address> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(UpdateAddressCommand command)
+		{
+			var values = await _repository.GetByIdAsync(command.AddressId);
+			values.Detail = command.Detail;
+			values.District = command.District;
+			values.District = command.District;
+			values.City = command.City;
+			values.UserId = command.UserId;
+
+			await _repository.UpdateAsync(values);
+
+		}
+	}
+}
---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Persistence.Context
+{
+	public class OrderContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=TUNAHAN\\SQLEXPRESS;Database=MultiShopOrder>Db;Integrated Security=true;TrustServerCertificate=true");
+		}
+		public DbSet<Address> Addresses { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+		public DbSet<Ordering> Orderings { get; set; }
+	}
+}
